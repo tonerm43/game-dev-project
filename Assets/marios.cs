80f58794782adeb7f8252676a2c95733c4bedcec
Mackenzie Jones <mackenzie.jones@unb.ca>
2020-01-25 14:22:12 -0400

    amrio bros

---
+++ Assets/marios.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using rand = UnityEngine.Random;
+
+public class marios: MonoBehaviour {
+
+    Rigidbody rb;
+    public int spinStrength;
+    public float forceFrequency; // times per second
+    public float spawnFrequency; 
+
+
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+        StartCoroutine(force());
+        StartCoroutine(spawn());
+
+        Physics.gravity = new Vector3(0, -1, 0);
+    }
+
+    IEnumerator force() {
+        while (true) {
+            addSpin();
+            yield return new WaitForSeconds(1/ forceFrequency);
+        }
+    }
+    private void addSpin() {
+        rb.AddTorque(new Vector3(rand.value * spinStrength, rand.value * spinStrength, rand.value * spinStrength));
+    }
+
+    IEnumerator spawn() {
+        while (true) {
+            spawnMario();
+            yield return new WaitForSeconds(1 / spawnFrequency);
+        }
+    }
+
+    private void spawnMario() {
+        GameObject newMario = Instantiate(gameObject, transform.position, transform.rotation);
+        Destroy(newMario.GetComponent<marios>());
+        newMario.AddComponent<spawnedMario>();
+    }
+}
---
+++ Assets/spawnedMario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnedMario: MonoBehaviour {
+
+    int secondsAlive;
+
+    void Start() {
+        Rigidbody newMarioRB = gameObject.GetComponent<Rigidbody>();
+        newMarioRB.constraints = RigidbodyConstraints.FreezePositionZ;
+        newMarioRB.useGravity = true;
+
+        secondsAlive = 15;
+        StartCoroutine(die());
+    }
+
+    IEnumerator die() {
+        if (secondsAlive > 0) {
+            yield return new WaitForSeconds(secondsAlive);
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate() {
+        if (transform.position.y < -10) {
+            Destroy(gameObject);
+        }
+    }
+}
